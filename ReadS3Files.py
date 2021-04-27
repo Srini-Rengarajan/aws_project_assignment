66db95b6ce9d0d7d43c7b6746bc0d304d30017fe
EC2 Default User <ec2-user@ip-172-31-5-158.us-east-2.compute.internal>
2021-04-27 09:59:08 +0000

    HandledNullValues

--- ReadS3Files.py
+++ ReadS3Files.py
@@ -5,6 +5,9 @@ from flask import *
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    return 'Welcome to S3 Demo Project!'
 
 @app.route("/list",methods = ['GET'])
 def listFilesInS3Bucket():
@@ -15,17 +18,29 @@ def listFilesInS3Bucket():
     #for file in my_bucket.objects.all():
         #print(file.key)
         
-    ky=""
+    keys=[]
+
     prefix=request.args.get('prefix')
 
     your_bucket = s3.Bucket('1834-testbucket')
-    
-    for s3_file in your_bucket.objects.filter(Prefix=prefix+'/'):
-        ky=s3_file.key
+   
+    if prefix:
+
+        for s3_file in your_bucket.objects.filter(Prefix=prefix+'/'):
         
+            keys.append(s3_file.key)
+    
+        if len(keys)==0:
+
+            return "No Objects found with the key "+prefix
+    
+    else:
+
+        return "No prefix provided! Kindly provide a prefix"
+
     #response = s3.list_objects(Bucket=your_bucket, MaxKeys=1)    
    
-    return ky
+    return jsonify(keys)
 
 
 if __name__ == '__main__':
